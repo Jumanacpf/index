41aea6d32388a3be0ce354084455146c2ee0ba36
jumanacpf <jumanacpf@gmail.com>
2024-03-21 11:25:39 +0530

    index.js

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-const input = document.getElementById("text-box")
-btn.addEventListener("click",function(e){
-    let text = input.value 
-        if(text===""){
-            alert("EMPTY!!!")
-        }else{
-        const newtask=document.createElement("p")
-        newtask.classList.add("task")
+const input = document.getElementById("text-box");
+const btn = document.getElementById("btn");
+const tasks = document.getElementById("tasks");
 
-        newtask.addEventListener('dblclick', () =>{
-            newtask.classList.add('line-through')
-            
-         })
-        let addvalue = document.createElement("span")
-            addvalue.innerHTML = text
-            newtask.appendChild(addvalue)
-        
-        let toEdit =document.createElement("button")
-            toEdit.classList.add("editt")
-            toEdit.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>'
-            newtask.appendChild(toEdit)
+btn.addEventListener("click", function(e) {
+    try {
+        let text = input.value;
 
-        let toDelete =document.createElement("button")
-            toDelete.classList.add("editt")
-            toDelete.innerHTML = '<i class="fa-solid fa-trash"></i>'
-            newtask.appendChild(toDelete)
+        if (text === "") {
+            throw new Error("Task is empty!");
+        }
 
+        const newtask = document.createElement("p");
+        newtask.classList.add("task");
 
-            toEdit.addEventListener("click",()=>{
-                    let val=prompt("Edit please")
-                    console.log(val)
-                    addvalue.innerHTML=val
-                })
+        newtask.addEventListener('dblclick', () => {
+            newtask.classList.add('line-through');
+        });
 
-                toDelete.addEventListener("click",()=>{
-                    newtask.remove()
-                })
+        let addvalue = document.createElement("span");
+        addvalue.innerHTML = text;
+        newtask.appendChild(addvalue);
 
+        let toEdit = document.createElement("button");
+        toEdit.classList.add("editt");
+        toEdit.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
+        newtask.appendChild(toEdit);
 
-       tasks.appendChild(newtask)
+        let toDelete = document.createElement("button");
+        toDelete.classList.add("editt");
+        toDelete.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        newtask.appendChild(toDelete);
+
+        toEdit.addEventListener("click", () => {
+            let val = prompt("Edit please");
+            if (val !== null) {
+                addvalue.innerHTML = val;
+            }
+        });
+
+        toDelete.addEventListener("click", () => {
+            newtask.remove();
+        });
+
+        tasks.appendChild(newtask);
+
+        input.value = "";
+    } catch (error) {
+        alert(error.message);
     }
-    input.value=""  
-})
+});
